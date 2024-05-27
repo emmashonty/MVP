e5c5ae1bce3ca475568c2b540f26f95a58fee952
Joseph Emmanuel <102731916+emmashonty@users.noreply.github.com>
2024-05-27 09:54:25 +0200

    Add files via upload

---
+++ MVP/MVP/MVP.iOS/AppDelegate.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Foundation;
+using UIKit;
+using Microsoft.Mvpui;
+using Xamarin.Forms.Platform.iOS;
+using Social;
+
+namespace Microsoft.Mvp.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            var tint = UIColor.FromRGB(0, 79, 138);
+
+            // Some basic navigationbar styling.
+            UINavigationBar.Appearance.BarTintColor = UIColor.FromRGB(250, 250, 250); //bar background
+            UINavigationBar.Appearance.TintColor = tint; //Tint color of button items
+
+            UIBarButtonItem.Appearance.TintColor = tint; //Tint color of button items
+
+            UITabBar.Appearance.TintColor = tint;
+
+            UISwitch.Appearance.OnTintColor = tint;
+
+            UIAlertView.Appearance.TintColor = tint;
+
+            UIView.AppearanceWhenContainedIn(typeof(UIAlertController)).TintColor = tint;
+            UIView.AppearanceWhenContainedIn(typeof(UIActivityViewController)).TintColor = tint;
+            UIView.AppearanceWhenContainedIn(typeof(SLComposeViewController)).TintColor = tint;
+
+            global::Xamarin.Forms.Forms.Init();
+
+            ImageCircle.Forms.Plugin.iOS.ImageCircleRenderer.Init();
+
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
---
+++ MVP/MVP/MVP.iOS/CustomControls/SignOutHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Mvp.iOS.Renders;
+using Microsoft.Mvpui;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Microsoft.Mvp.Interfaces;
+
+[assembly: Dependency(typeof(SignOutHelper))]
+namespace Microsoft.Mvp.iOS.Renders
+{
+    public class SignOutHelper : ISignOutHelper
+    {
+        public void CloseApp()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
